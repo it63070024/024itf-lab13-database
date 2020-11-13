ad57df5a361e176d696929c8b622a244671067d3
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-13 15:48:22 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -8,7 +8,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-dark">
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'it63070024-itf-lab13-database-php.mysql.database.azure.com', 'it630070024@it63070024-itf-lab13-database-php', 'TMRpti62', 'itflab', 3306);

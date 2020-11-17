0659449f501bd20a943e8ddc7efcf8c386f70cc8
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-17 09:22:24 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'it63070024-itf-lab13-database-php.mysql.database.azure.com', 'it630070024@it63070024-itf-lab13-database-php', 'TMRpti62', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
-{(
+{
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 

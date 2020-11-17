1e1e15783278a828b42f7ca649915e6ff91f4cc4
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-17 09:29:12 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -17,9 +17,10 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    header("Location: index.php");
+    exit();
   } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Something went wrong: " . $sql . "<br>" . mysqli_error($conn);
   }
   
 mysqli_close($conn);

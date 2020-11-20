1f03e560ddaa6d27d83a44ec76c59ec356c8cb89
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-20 22:54:09 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,36 +1,168 @@
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'it63070024-itf-lab13-database-php.mysql.database.azure.com', 'it630070024@it63070024-itf-lab13-database-php', 'TMRpti62', 'itflab', 3306);
-if (mysqli_connect_errno($conn)){
-	die('Failed to connect to MySQL: '.mysqli_connect_error());
+
+require_once "config.php";
+ 
+
+$name = $comment = $link = "";
+$name_err = $comment = $link_err = "";
+ 
+
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+
+    $id = $_POST["id"];
+    
+
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+
+    $input_comment = trim($_POST["comment"]);
+    if(empty($input_comment)){
+        $comment_err = "Write Something";     
+    } else{
+        $comment = $input_comment;
+    }
+    
+
+    $input_link = trim($_POST["link"]);
+    if(empty($input_link)){
+        $link_err = "Please enter your link.";     
+    } else{
+        $link = $input_link;
+    }
+    
+    if(empty($name_err) && empty($comment_err) && empty($link_err)){
+
+        $sql = "UPDATE guestbook SET name=?, comment=?, link=? WHERE id=?";
+         
+        if($stmt = mysqli_prepare($conn, $sql)){
+
+            mysqli_stmt_bind_param($stmt, "sssi", $param_name, $param_comment, $param_link, $param_id);
+            
+
+            $param_name = $name;
+            $param_comment = $address;
+            $param_link = $link;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM guestbook WHERE id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+               a if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as n associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $name = $row["name"];
+                    $address = $row["address"];
+                    $salary = $row["salary"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($conn);
+    }  else{
+
+        header("location: error.php");
+        exit();
+    }
 }
-
-$id = $_GET['edit'];
-$result = mysqli_query($conn, "SELECT* FROM guestbook WHERE id=$id");
-$row = mysqli_fetch_array($result);
 ?>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<div class="container">
-	<div class="row">
-		<div  class="col-sm-5">
-			<br>
-			<h3>Edit Comment</h3>
-			<form action="insert_edit.php" method="post">
-			<input type="hidden" name="id" value="<?php echo $id; ?>">
-				<div class="form-group">
-				Name : <input type="text" name="name" required value="<?php echo $row['name'];?>" class="form-control" value="$row['name']"> 
-				</div>
-				<div class="form-group">
-				Comment : <input type="texl" name="comment" required value="<?php echo $row['comment'];?>" class="form-control" >
-			</div>
-				<div class="form-group">
-				Link : <input type="text" name="link" required value="<?php echo $row['link'];?>" class="form-control" >
-				</div>
-				<div class="form-group">
-				<button type="submit" value="<?php echo $row['id'];?>"name="id" class="btn btn-success">Save</button>
-			</div>
-			</form>
-		</div>
-	</div>
-</div>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit Comment</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Edit Comment</h2>
+                    </div>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($comment_err)) ? 'has-error' : ''; ?>">
+                            <label>Comment</label>
+                            <textarea name="comment" class="form-control"><?php echo $comment; ?></textarea>
+                            <span class="help-block"><?php echo $comment_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($link_err)) ? 'has-error' : ''; ?>">
+                            <label>Link</label>
+                            <input type="text" name="link" class="form-control" value="<?php echo $link; ?>">
+                            <span class="help-block"><?php echo $link_err;?></span>
+                        </div>
+                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>

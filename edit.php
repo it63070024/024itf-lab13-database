cc996013238e67323025d24764ccf1439f8bee01
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-24 11:07:37 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -46,7 +46,7 @@ if(isset($_POST['save'])){
 	$comment = $_POST['comment'];
 	$link = $_POST['link'];
 
-	$sql = "UPDATE guestbook SET Name=$name, Comment=$comment, Link=$link WHERE ID=$id" ;
+	$sql = "UPDATE guestbook SET ID=$id, name=$name, comment=$comment, link=$link WHERE ID=$id" ;
 
 
 	if (mysqli_query($conn, $sql)) {

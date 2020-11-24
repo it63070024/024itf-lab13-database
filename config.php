2fa219e8ad7cc80214e401af47f374b7175aa370
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-24 11:33:30 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-$conn = mysqli_real_connect($conn, 'it63070024-itf-lab13-database-php.mysql.database.azure.com', 'it630070024@it63070024-itf-lab13-database-php', 'TMRpti62', 'itflab', 3306);
- 
-if($conn === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>

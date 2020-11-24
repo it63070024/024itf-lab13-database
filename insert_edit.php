d7480e98a70d3eefebd38152c9d914c01b8a9592
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-24 11:33:18 +0700

    Add files via upload

--- insert_edit.php
+++ insert_edit.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$id = $_POST['id'];
+$id = $_POST['ID'];
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];

2d56e2741d26ebd993dcd56cbc633eaece13a4a8
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-24 11:20:18 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno($conn)){
 }
 
 $id = $_GET['id'];
-$result = mysqli_query($conn, 'SELECT * FROM guestbook WHERE ID=$id');
+$result = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID=$id");
 $row = mysqli_fetch_array($result);
 ?>
 
@@ -19,9 +19,9 @@ $row = mysqli_fetch_array($result);
 			<h3>Edit Comment</h3>
 		
 
-			<form action="" method="post">
+			<form action="insert_edit.php" method="post">
 				<div class="form-group">
-					<input type="hidden" name="id" value="<?php echo $row['ID'];?>" class="form-control" > 
+					<input type="hidden" name="id" class="form-control" > 
 				</div>
 				<div class="form-group">
 					Name : <input type="text" name="name" required value="<?php echo $row['Name'];?>" class="form-control" > 
@@ -33,7 +33,7 @@ $row = mysqli_fetch_array($result);
 					Link : <input type="text" name="link" required value="<?php echo $row['Link'];?>" class="form-control" >
 				</div>
 				<div class="form-group">
-					<button name="save" value="<?php echo $row['ID'];?>" class="btn btn-success" type="submit" >Save</button>
+					<button action="<?php echo $row['ID'];?>" name="save" value="<?php echo $row['ID'];?>" class="btn btn-success" type="submit" >Save</button>
 				</div>
 			</form>
 		</div>
@@ -41,20 +41,3 @@ $row = mysqli_fetch_array($result);
 	</div>
 </div>
 <?php
-if(isset($_POST['save'])){
-	$name = $_POST['name'];
-	$comment = $_POST['comment'];
-	$link = $_POST['link'];
-
-	$sql = "UPDATE guestbook SET Name=$name, Comment=$comment, Link=$link WHERE ID=$id" ;
-
-
-	if (mysqli_query($conn, $sql)) {
-		header("Location:index.php");
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	}
-		mysqli_close($conn);
-}
-
-?>

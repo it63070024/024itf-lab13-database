d96d16e584ebd80fe7b10e18b8dcd814db311405
it63070024 <73686325+it63070024@users.noreply.github.com>
2020-11-24 11:16:22 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno($conn)){
 }
 
 $id = $_GET['id'];
-$result = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID=$id");
+$result = mysqli_query($conn, 'SELECT * FROM guestbook WHERE ID=$id');
 $row = mysqli_fetch_array($result);
 ?>
 
@@ -24,13 +24,13 @@ $row = mysqli_fetch_array($result);
 					<input type="hidden" name="id" value="<?php echo $row['ID'];?>" class="form-control" > 
 				</div>
 				<div class="form-group">
-					Name : <input type="text" name="gname" required value="<?php echo $row['Name'];?>" class="form-control" > 
+					Name : <input type="text" name="name" required value="<?php echo $row['Name'];?>" class="form-control" > 
 				</div>
 				<div class="form-group">
-					Comment : <input type="text" name="gcomment" required value="<?php echo $row['Comment'];?>" class="form-control" >
+					Comment : <input type="text" name="comment" required value="<?php echo $row['Comment'];?>" class="form-control" >
 				</div>
 				<div class="form-group">
-					Link : <input type="text" name="glink" required value="<?php echo $row['Link'];?>" class="form-control" >
+					Link : <input type="text" name="link" required value="<?php echo $row['Link'];?>" class="form-control" >
 				</div>
 				<div class="form-group">
 					<button name="save" value="<?php echo $row['ID'];?>" class="btn btn-success" type="submit" >Save</button>
@@ -42,9 +42,9 @@ $row = mysqli_fetch_array($result);
 </div>
 <?php
 if(isset($_POST['save'])){
-	$name = $_POST['gname'];
-	$comment = $_POST['gcomment'];
-	$link = $_POST['glink'];
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$link = $_POST['link'];
 
 	$sql = "UPDATE guestbook SET Name=$name, Comment=$comment, Link=$link WHERE ID=$id" ;
 
